28231d6cbb8e07d0076e773a1e5fe7a6be2fc674
Anatolii Fenin <fenintolja@gmail.com>
2025-03-12 10:58:19 +0200

    lease mvc

---
+++ server/src/controllers/leaseControllers.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
+export const prisma = new PrismaClient();
+
+export const getLeases = async (_: Request, res: Response) => {
+  try {
+    const leases = await prisma.lease.findMany({
+      include: { tenant: true, property: true },
+    });
+
+    res.json(leases);
+  } catch (error) {
+    res.status(500).json({
+      message: `Error retrieving leases: ${(error as Error)?.message}`,
+    });
+  }
+};
+
+export const getLeasePayments = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const payments = await prisma.payment.findMany({
+      where: { leaseId: Number(id) },
+    });
+    res.json(payments);
+  } catch (error) {
+    res.status(500).json({
+      message: `Error retrieving lease payments: ${(error as Error)?.message}`,
+    });
+  }
+};
